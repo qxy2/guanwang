93e9cfdad45826023eada9afc80a083643388744
qxy2 <705698086@qq.com>
2016-11-09 16:50:27 +0800

    add code

---
+++ shouye.js
@@ -0,0 +1,57 @@
+
+function show(obj){
+	var id = obj.id;
+	//alter(id);
+	if(id=="a"){
+		obj.src="shun21.png";
+		var img = document.getElementById("yemian");
+		img.src="91.png";
+	}
+	
+	else if(id=="b"){
+		obj.src="shun22.png";
+		var img = document.getElementById("yemian");
+		img.src="92.png";
+	}
+	
+	 else if(id=="c"){
+		obj.src="shun23.png";
+		var img = document.getElementById("yemian");
+		img.src="91.png";
+	}
+	
+	else if(id=="d"){
+		obj.src="shun24.png";
+		var img = document.getElementById("yemian");
+		img.src="94.png";
+	}
+	
+	else if(id=="e"){
+		obj.src="shun25.png";
+		var img = document.getElementById("yemian");
+		img.src="95.png";
+	} 
+}
+
+function likai(obj){
+     var id=obj.id;
+	 if(id=="a"){
+	   obj.src="shun31.png";
+	 }
+	 
+	 else if(id=="b"){
+	   obj.src="shun32.png";
+	 }
+	 
+	  else if(id=="c"){
+	   obj.src="shun33.png";
+	 }
+	 
+	 else if(id=="d"){
+	   obj.src="shun34.png";
+	 }
+	 
+	 else if(id=="e"){
+	   obj.src="shun35.png";
+	 } 
+}
